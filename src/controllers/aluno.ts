76dbde381d1a3d185029b6bd6a00a3dc36f3ca4c
Enzo Crisóstomo Nogueira <enzo.crisostomo2000@gmail.com>
2023-07-20 01:51:50 -0300

    Aluno

--- src/controllers/aluno.ts
+++ src/controllers/aluno.ts
@@ -1,48 +1,97 @@
 import prisma from "../prisma";
 import { RequestHandler } from "express";
 import HttpError from "http-errors";
-import { RetrieveAlunoParams, SearchAlunoQuery } from "../schemas/aluno";
+import {
+  DetailAlunoParams,
+  DetailAlunoQueryResult,
+  SearchAlunoQuery,
+  SearchAlunoQueryResult,
+} from "../schemas/aluno";
 import { mountSearchSet } from "../utils";
 
 const search: RequestHandler = async (req, res, next) => {
-  const { nome, curso, curriculo, periodoIngresso, status, _offset, _size } =
+  const { nome, curso, curriculo, periodoIngresso, _offset, _size } =
     SearchAlunoQuery.parse(req.query);
 
-  const _total = await prisma.sIGAA_ALUNO.count({
-    where: {
-      NOME: {
-        contains: nome,
-        mode: "insensitive",
-      },
-    },
-  });
+  const alunos = SearchAlunoQueryResult.parse(
+    await prisma.$queryRaw`
+        select
+          count(alu."MATRICULA") over() as _total,
+          alu."MATRICULA" as id,
+          alu."MATRICULA" as matricula,
+	        alu."NOME" as nome
+        from "SIGAA_ALUNO" alu
+        inner join "SIGAA_RL_ALUNO_CURSO" ac ON alu."MATRICULA" = ac."ALUNO" 
+        where (unaccent(alu."NOME") ilike '%'||unaccent(${nome}::text)||'%' or ${nome}::text is null) and
+              (unaccent(ac."CURSO") ilike '%'||unaccent(${curso}::text)||'%' or ${curso}::text is null) and
+              (unaccent(ac."CURRICULO") ilike '%'||unaccent(${curriculo}::text)||'%' or ${curriculo}::text is null) and
+              (substring(ac."PERIODO_LETIVO_REGISTRO" from 1 for 4) = ${
+                periodoIngresso?.split("/")[0]
+              }::text or ${periodoIngresso}::text is null) and
+              (substring(ac."PERIODO_LETIVO_REGISTRO" from 5) = ${
+                periodoIngresso?.split("/")[1]
+              }::text or ${periodoIngresso}::text is null)
+        order by alu."NOME" 
+        offset ${_offset}::integer
+        limit ${_size}::integer`
+  );
 
-  const alunos = await prisma.sIGAA_ALUNO.findMany({
-    where: {
-      NOME: {
-        contains: nome,
-        mode: "insensitive",
-      },
-    },
-    orderBy: { MATRICULA: "asc" },
-    skip: _offset,
-    take: _size,
-  });
+  const _total = Number(alunos[0]?._total ?? 0);
 
-  return res.json(mountSearchSet(alunos, _offset, _total));
+  const alunosTransformed = alunos.map((item) => ({
+    id: item.id,
+    matricula: item.matricula,
+    nome: item.nome,
+  }));
+
+  return res.json(mountSearchSet(alunosTransformed, _offset, _total));
 };
 
 const retrieve: RequestHandler = async (req, res, next) => {
-  const { matricula } = RetrieveAlunoParams.parse(req.params);
+  const { matricula } = DetailAlunoParams.parse(req.params);
 
-  const aluno = await prisma.sIGAA_ALUNO.findUnique({
-    where: { MATRICULA: matricula },
-    include: {
-      SIGAA_RL_ALUNO_CURSO: true,
+  const aluno = DetailAlunoQueryResult.parse(
+    await prisma.$queryRaw`
+    select
+      alu."MATRICULA" as id,
+      alu."MATRICULA" as matricula,
+      alu."NOME" as nome,
+      ac."CURSO" as cod_curso,
+      cur."NOME" as "nome_curso",
+      ac."CURRICULO" as curriculo,
+      ac."IRA" as ira,
+      substring(ac."PERIODO_LETIVO_REGISTRO" from 1 for 4) as "ano_ingresso",
+      substring(ac."PERIODO_LETIVO_REGISTRO" from 5) as "numero_ingresso"
+    from "SIGAA_ALUNO" alu
+    inner join "SIGAA_RL_ALUNO_CURSO" ac ON alu."MATRICULA" = ac."ALUNO" 
+    left join "SIGAA_CURSO" cur on ac."CURSO" = cur."ID"
+    where alu."MATRICULA" = ${matricula}::text`
+  );
+
+  if (!aluno[0]) {
+    throw new HttpError.NotFound(`Aluno [${matricula}] não encontrado!`);
+  }
+
+  const alunoTransformed = {
+    id: aluno[0].id,
+    matricula: aluno[0].matricula,
+    nome: aluno[0].nome,
+    email: aluno[0].nome.replace(" ", ".").toLocaleLowerCase() + "@aluno.unb.br",
+    ira: Number(aluno[0].ira.toPrecision(3)),
+    curriculo: aluno[0].curriculo,
+    status: true,
+    periodoIngresso: {
+      ano: aluno[0].ano_ingresso,
+      numero: aluno[0].numero_ingresso
     },
-  });
+    curso: {
+      id: aluno[0].cod_curso,
+      codigo: aluno[0].cod_curso,
+      nome: aluno[0].nome_curso
+    }
+  }
 
-  return res.json(aluno);
+  return res.json(alunoTransformed);
 };
 
 export default {
--- src/schemas/aluno.ts
+++ src/schemas/aluno.ts
@@ -4,12 +4,43 @@ export const SearchAlunoQuery = z.object({
   nome: z.string().optional(),
   curso: z.string().optional(),
   curriculo: z.string().optional(),
-  periodoIngresso: z.string().optional(),
-  status: z.boolean().optional(),
-  _offset: z.coerce.number().min(0).optional(),
-  _size: z.coerce.number().min(1).optional(),
+  periodoIngresso: z
+    .string()
+    .regex(
+      RegExp("[0-9]{0,4}/[0-9]?"),
+      "Padrão: [ano]/[periodo] Ex: '2019/2'\n"
+    )
+    .optional(),
+  _offset: z.coerce
+    .number()
+    .min(0, "_offset não pode ser menor que 0.")
+    .optional(),
+  _size: z.coerce.number().min(1, "_size não pode ser menor que 1.").optional(),
 });
 
-export const RetrieveAlunoParams = z.object({
+export const SearchAlunoQueryResult = z.array(
+  z.object({
+    _total: z.bigint().min(BigInt(0)),
+    id: z.string(),
+    matricula: z.string(),
+    nome: z.string(),
+  })
+);
+
+export const DetailAlunoParams = z.object({
   matricula: z.coerce.string(),
 });
+
+export const DetailAlunoQueryResult = z.array(
+  z.object({
+    id: z.string(),
+    matricula: z.string(),
+    nome: z.string(),
+    cod_curso: z.string(),
+    nome_curso: z.string(),
+    curriculo: z.string(),
+    ira: z.number(),
+    ano_ingresso: z.coerce.number(),
+    numero_ingresso: z.coerce.number(),
+  })
+);
