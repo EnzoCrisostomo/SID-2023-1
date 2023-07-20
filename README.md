1c044a20e8645f19f4e5d06699a4d1fc9c74200d
Enzo Crisóstomo Nogueira <enzo.crisostomo2000@gmail.com>
2023-07-19 23:08:10 -0300

    Documentar guia inicial

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
-# SID
+# SID-2023-1
 
-# Script npm para iniciar o servidor:
+# Quick start
+## Preparar ambiente
+Copiar o arquivo exmample.env e renomear para ```.env```. 
+
+Caso necessário alterar seu conteúdo de acordo com o endereço do seu banco de dados e a porta desejada para o backend.
+
+## Instalar pacotes
+```bash
+npm install
+```
+## Preparar banco com prisma
+```bash
+npm run migrate-dev
+```
+
+## Iniciar servidor
+Isso também irá construir o arquivo swagger unificado
 ```bash
 npm start
 ```
+
+## Verificar swagger
+O servidor hospeda uma página web contendo o swagger em [http://localhost:3333/SIGAA/api/matriculasigaa/Docs](http://localhost:3333/SIGAA/api/matriculasigaa/Docs)
+
 
 # Scripts npm para dar merge de todos os arquivos em apenas um:
 ## Json
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,17 @@ import prisma from "./prisma";
 import handlePrismaError from "./middlewares/handlePrismaError";
 import handleCommonError from "./middlewares/handleCommonError";
 import handleZodError from "./middlewares/handleZodError";
+import path from "path";
+import dotenv from "dotenv-safe";
+
+const PROJECT_ROOT = path.resolve(__dirname, "..");
+
+dotenv.config({
+  example: path.resolve(PROJECT_ROOT, ".example.env"),
+  path: path.resolve(PROJECT_ROOT, ".env"),
+});
+
+const BACKEND_PORT = process.env.BACKEND_PORT as string;
 
 const app = express();
 
@@ -22,8 +33,7 @@ app.use(handleZodError);
 app.use(handlePrismaError);
 app.use(handleCommonError);
 
-app.listen(3333, async () => {
-    await prisma.$connect();
-    console.log("Server is running in http://localhost:3333");
+app.listen(BACKEND_PORT, async () => {
+  await prisma.$connect();
+  console.log(`Server is running in http://localhost:${BACKEND_PORT}`);
 });
-
