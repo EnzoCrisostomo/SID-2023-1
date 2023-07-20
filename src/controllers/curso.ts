2f195da48e547f33e5bcf4069fe2811fb52cbfa0
Enzo Crisóstomo Nogueira <enzo.crisostomo2000@gmail.com>
2023-07-20 01:57:13 -0300

    esqueleto curso

--- src/controllers/curso.ts
+++ src/controllers/curso.ts
@@ -3,11 +3,29 @@ import { RequestHandler } from "express";
 import HttpError from "http-errors";
 
 const search: RequestHandler = async (req, res, next) => {
-  const cursos = await prisma.sIGAA_CURSO.findMany({});
-
-  return res.json(cursos);
+  return res.json({});
+};
+const detail: RequestHandler = async (req, res, next) => {
+  return res.json({});
+};
+const searchEstruturaCurricular: RequestHandler = async (req, res, next) => {
+  return res.json({});
+};
+const detailEstruturaCurricular: RequestHandler = async (req, res, next) => {
+  return res.json({});
+};
+const searchDisciplinasEstruturaCurricular: RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  return res.json({});
 };
 
 export default {
   search,
+  detail,
+  searchEstruturaCurricular,
+  detailEstruturaCurricular,
+  searchDisciplinasEstruturaCurricular,
 };
--- src/routes/api/curso.ts
+++ src/routes/api/curso.ts
@@ -4,5 +4,9 @@ import CursoController from "../../controllers/curso";
 const cursoRouter = Router();
 
 cursoRouter.get("/", CursoController.search);
+cursoRouter.get("/:id", CursoController.detail);
+cursoRouter.get("/:id/EstruturaCurricular", CursoController.searchEstruturaCurricular);
+cursoRouter.get("/:id/EstruturaCurricular/:curriculo", CursoController.detailEstruturaCurricular);
+cursoRouter.get("/:id/EstruturaCurricular/:curriculo/disciplina", CursoController.searchDisciplinasEstruturaCurricular);
 
 export default cursoRouter;
